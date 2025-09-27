6b7feaafe7c52eebd198bbffb7dfc3bf766609e6
shristitechacademy <sripriyamathan@gmail.com>
2025-09-27 16:19:05 +0530

    basic spring

---
+++ SpringBasics/src/main/java/com/spring/constr/Student.java
@@ -0,0 +1,41 @@
+package com.spring.constr;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	
+	private String studentName;
+	private String department;
+	private Course course;
+	
+	public Student(Course course) {
+		super();
+		this.course = course;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+	@Value("CSE")
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+	@Value("Sri")
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentName=" + studentName + ", department=" + department + ", course=" + course + "]";
+	}
+
+	
+	
+}
