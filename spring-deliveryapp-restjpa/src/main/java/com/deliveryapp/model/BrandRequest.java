10a14d497ad328d56c4ac27e811e334122331382
shristitechacademy <sripriyamathan@gmail.com>
2025-11-08 12:02:15 +0530

    rest api

---
+++ spring-deliveryapp-restjpa/src/main/java/com/deliveryapp/model/BrandRequest.java
@@ -0,0 +1,19 @@
+package com.deliveryapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BrandRequest {
+	private Integer brandId;
+	private String brandName; 
+}
+
+
+
+
+
+
